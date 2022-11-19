7ee855fe1bd73e184caaa0ab99a4bc079caa2fd2
Zhuravlev84 <zyzy84@rambler.ru>
2022-11-19 05:41:01 +0300

    finish commit

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -6,26 +6,27 @@
 7 -> да
 1 -> нет */
 
-WriteOn("Введите число дня недели: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+int number1 = ReadOn("Введите число дня недели: ");
+
 if (number1 == 1 || number1 == 2 || number1 == 3 || number1 == 4 
 || number1 == 5)
 {
-    WriteOn("День не является выходным");
+    Console.Write("День не является выходным");
 }
 
 else if (number1 == 6 || number1 == 7)
 {
-    WriteOn("День является выходным");
+    Console.Write("День является выходным");
 }
 
 else
 {
-    WriteOn ("Некорректное число, введите от 1 до 7");
+    Console.Write("Некорректное число, введите от 1 до 7");
 }
 
-void WriteOn (string messege)
+int ReadOn (string messege)
     {
         Console.Write(messege);
+        return Convert.ToInt32(Console.ReadLine());
     }
     
